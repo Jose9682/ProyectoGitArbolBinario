259ad6ed3481f2db06a175c5581e0fceba4a6c27
Jose Medina <josemedina1983@gmail.com>
2017-07-02 19:36:35 -0300

    cambios de clase empleado

--- Arbol/src/Empleado.java
+++ Arbol/src/Empleado.java
@@ -1,6 +1,32 @@
 
 public class Empleado {
-	Integer id;
-	//cambios de git
-	// solo commit
+	private Integer id;
+	private String desc;
+	private String desc2;
+	private String desc3;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getDesc2() {
+		return desc2;
+	}
+	public void setDesc2(String desc2) {
+		this.desc2 = desc2;
+	}
+	public String getDesc3() {
+		return desc3;
+	}
+	public void setDesc3(String desc3) {
+		this.desc3 = desc3;
+	}
+	
 }
