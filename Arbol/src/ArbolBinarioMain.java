373d79c491e24e8bb694293f6a95474190c01243
Jose Medina <josemedina1983@gmail.com>
2017-07-02 21:18:21 -0300

    kfd

--- Arbol/src/ArbolBinarioMain.java
+++ Arbol/src/ArbolBinarioMain.java
@@ -26,9 +26,8 @@ public class ArbolBinarioMain {
 		
 		Arrays.asList(e1, e2, e3, e4, e5, e6).forEach(bst :: insertar);
 		bst.preorden();
-		bst.postorden();//colocado para el cambio desde otro branch
+		bst.postorden();
 	}
-	
 	
 private static void buscar (BST bst, int id){
 	if (bst.existe(id)) {
