37b088d8d76b809590a35358c53d2b18090e511d
Jose <jose.medina@sonda.com>
2017-07-07 10:30:20 -0400

    cambios eliminar elemento de arbol

--- Arbol/src/ArbolBinarioMain.java
+++ Arbol/src/ArbolBinarioMain.java
@@ -3,38 +3,40 @@ import java.util.Arrays;
 
 public class ArbolBinarioMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Empleado e1 = new Empleado(44, "Jose Medina", "otra cosa", "otro cosa");
-		Empleado e2 = new Empleado(34, "Jose Medina", "otra cosa", "otro cosa");
-		Empleado e3 = new Empleado(84, "Jose Medina", "otra cosa", "otro cosa");
-		Empleado e4 = new Empleado(74, "Jose Medina", "otra cosa", "otro cosa");
-		Empleado e5 = new Empleado(54, "Jose Medina", "otra cosa", "otro cosa");
-		Empleado e6 = new Empleado(14, "Jose Medina", "otra cosa", "otro cosa");
-		
-		BST bst = new BST();
-	/*	System.out.println("Esta vacio: " + bst.esVacio() + " - Es hoja: " + bst.esHola());
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Empleado e1 = new Empleado(44, "Jose Medina", "otra cosa", "otro cosa");
+        Empleado e2 = new Empleado(34, "Jose Medina", "otra cosa", "otro cosa");
+        Empleado e3 = new Empleado(35, "Jose Medina", "otra cosa", "otro cosa");
+        Empleado e4 = new Empleado(55, "Jose Medina", "otra cosa", "otro cosa");
+        Empleado e5 = new Empleado(15, "Jose Medina", "otra cosa", "otro cosa");
+        Empleado e6 = new Empleado(14, "Jose Medina", "otra cosa", "otro cosa");
+
+        BST bst = new BST();
+        /*	System.out.println("Esta vacio: " + bst.esVacio() + " - Es hoja: " + bst.esHola());
 		bst.insertar(e1);
 		System.out.println("Esta vacio: " + bst.esVacio() + " - Es hoja: " + bst.esHola());
 		bst.insertar(e2);
 		System.out.println("Esta vacio: " + bst.esVacio() + " - Es hoja: " + bst.esHola());
-		
-		
+
+
 		buscar(bst, 44);
 		buscar(bst, 8);
-		*/
-		
-		Arrays.asList(e1, e2, e3, e4, e5, e6).forEach(bst :: insertar);
-		bst.preorden();
-		bst.postorden();//colocado para el cambio desde otro branch
-	}
-	
-	
-private static void buscar (BST bst, int id){
-	if (bst.existe(id)) {
-		System.out.println(bst.obtener(id));
-	} else {
-		System.out.println("No se encuentra el empleado id: "+ id);
-	}
-}
+         */
+
+        Arrays.asList(e1, e2, e3, e4, e5, e6).forEach(bst :: insertar);
+        bst.preorden();
+        //colocado para el cambio desde otro branch
+        bst.eliminar(44);
+        bst.preorden();
+    }
+
+
+    private static void buscar (BST bst, int id){
+        if (bst.existe(id)) {
+            System.out.println(bst.obtener(id));
+        } else {
+            System.out.println("No se encuentra el empleado id: "+ id);
+        }
+    }
 }
--- Arbol/src/BST.java
+++ Arbol/src/BST.java
@@ -1,9 +1,10 @@
+import javax.management.RuntimeErrorException;
 
 public class BST implements IBST<Empleado> {
 
 	private Empleado valor;
 	private BST izdo, dcho;
-
+	private BST padre;
 
 
 	@Override
@@ -15,24 +16,31 @@ public class BST implements IBST<Empleado> {
 	public boolean esVacio() {
 		return valor == null;
 	}
-	@Override
-	public void insertar(Empleado empl) {
+
+    @Override
+    public void insertar(Empleado empl) {
+        // TODO Auto-generated method stub
+        insertarImlp(empl, null);
+    }
+	private void insertarImlp(Empleado empl , BST padre) {
 		if (valor == null) {
 			this.valor = empl;
+			this.padre = padre;
 		} else {
 			if (empl.compareTo(valor) < 0) {
 				if (izdo == null) izdo = new BST(); 
-				izdo.insertar(empl);
+				izdo.insertarImlp(empl, this);
 			} else if (empl.compareTo(valor)>0){
 				if (dcho == null) dcho = new BST();
-				dcho.insertar(empl);
+				dcho.insertarImlp(empl, this);
 			} else {
-
+			    throw new RuntimeException("Error");
 			}
 		}
 
 	}
 
+	
 	@Override
 	public boolean existe(int id) {
 		// TODO Auto-generated method stub
@@ -114,10 +122,51 @@ public class BST implements IBST<Empleado> {
 		}
 	}
 
+	private BST minimo() {
+	    if (izdo != null && izdo.esVacio()) {
+            return izdo.minimo();
+        } else {
+            return this;
+        }
+	}
+	private void eliminarImpl(int id){
+	    
+	    if (izdo != null && dcho != null ) {
+	    //eliminar con 2 hijos   
+	        BST minimo = dcho.minimo();
+	        this.valor = minimo.valor;
+	        dcho.eliminar(minimo.valor.getId());
+	    } else if (izdo != null || dcho != null){
+	        //eliminar con 1 hijos
+	        BST sustituto = izdo != null ? izdo : dcho;
+	        this.valor = sustituto.valor;
+	        this.izdo = sustituto.izdo;
+	        this.dcho = sustituto.dcho;
+	    } else {
+	        if (padre != null) {
+	            if (this == padre.izdo) padre.izdo = null;
+	            if (this == padre.dcho) padre.dcho = null;
+	            padre = null;
+	        }
+	        valor = null;
+	    }
+	}
 	@Override
 	public void eliminar(int id) {
 		// TODO Auto-generated method stub
+	    if(valor != null){
+	        if (id == valor.getId()) {
+	            eliminarImpl(id);
+                
+            } else if (id < valor.getId() && izdo != null ){
+                izdo.eliminar(id);
+            } else if (id > valor.getId() && dcho != null){
+                dcho.eliminar(id);
+            }
+	        
+	    }
 
 	}
+
 
 }
